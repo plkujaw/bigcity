07fd46f5f53bee7e7096cab0414aca8d8067bfa4
plkujaw <kuba.jawor@gmail.com>
2020-08-20 18:45:32 +0100

    variable name change

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ import Appreciation from "./js/src/appreciation.js";
 
 const appreciateBtn = document.getElementById("appreciate");
 const appreciationCount = document.querySelector(".appreciations span");
-const appreciation = new Appreciation(parseInt(appreciationCount.textContent));
+
+const appreciations = new Appreciation(parseInt(appreciationCount.textContent));
 
 appreciateBtn.addEventListener("click", () => {
-  appreciation.addAppreciation();
-  appreciationCount.textContent = appreciation.count;
+  appreciations.addAppreciation();
+  appreciationCount.textContent = appreciations.count;
 });
