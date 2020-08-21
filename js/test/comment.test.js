ebcbc038586cb112862c8bf1bf49eb44d5a5bf19
plkujaw <kuba.jawor@gmail.com>
2020-08-21 13:26:23 +0100

    add test for comment to have an author

--- js/test/comment.test.js
+++ js/test/comment.test.js
@@ -4,3 +4,8 @@ test("comment should have a content", () => {
   const comment = new Comment("awesome content, well done!");
   expect(comment.content).toBe("awesome content, well done!");
 });
+
+test("comment should have an author", () => {
+  const comment = new Comment("awesome content, well done!", "username");
+  expect(comment.author).toBe("username");
+});
