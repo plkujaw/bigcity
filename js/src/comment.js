31a1b6ec5805e590f5d85e1e318b006591845b51
plkujaw <kuba.jawor@gmail.com>
2020-08-24 11:20:54 +0100

    add comment for clarity

--- js/src/comment.js
+++ js/src/comment.js
@@ -5,7 +5,7 @@ class Comment {
   }
   isEmpty() {
     const content = this.content;
-    const regex = /\S+/g;
+    const regex = /\S+/g; // matches any non-whitespace character
     const match = content.match(regex) || []; // we use || [] because match returns null if there is no match and throws an error
     if (match.length === 0) {
       return true;
