e28022a4f1e4a3ef85888dfa3ed660dc7c5b681d
plkujaw <kuba.jawor@gmail.com>
2020-09-11 12:40:35 +0100

    add comment button click shows/hides form

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import AppreciationsCount from "./js/src/appreciation.js";
+import Comment from "./js/src/comment.js";
 
-const appreciateBtn = document.getElementById("appreciate");
+const appreciateBtn = document.getElementById("appreciate-btn");
 const appreciationCount = document.querySelector(".appreciations span");
+
+const commentForm = document.querySelector(".comment-form-container");
+const commentBtn = document.getElementById("comment-btn");
+const submitComment = document.getElementById("submit-comment");
 
 const appreciations = new AppreciationsCount(
   parseInt(appreciationCount.textContent)
@@ -10,4 +15,8 @@ const appreciations = new AppreciationsCount(
 appreciateBtn.addEventListener("click", () => {
   appreciations.addAppreciation();
   appreciationCount.textContent = appreciations.count;
+});
+
+commentBtn.addEventListener("click", () => {
+  commentForm.classList.toggle("hide");
 });
