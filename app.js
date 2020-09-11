401eda58b7d108efa8712f767fab9994f149140c
plkujaw <kuba.jawor@gmail.com>
2020-09-11 13:56:56 +0100

    add addComment function

--- app.js
+++ app.js
@@ -23,5 +23,16 @@ commentBtn.addEventListener("click", () => {
 
 addCommentBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  addComment();
+  // displayComment();
   commentForm.classList.add("hide");
 });
+
+function addComment() {
+  const commentAuthor = document.getElementById("author").value;
+  // console.log(commentAuthor);
+  const commentContent = document.getElementById("comment-input-textarea")
+    .value;
+  // console.log(commentContent);
+  const comment = new Comment(commentContent, commentAuthor);
+}
